73378fe8969a447f49eb82750c69edf42abcb74d
Tony Cain <r.tonycain@gmail.com>
2020-08-17 19:14:23 -0400

    routing lesson up date

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
-import Welcome from "./components/welcome/Welcom";
+import React, {Component} from 'react';
+import Welcome from './components/welcome/Welcom';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+import { Route } from 'react-router-dom';
 
-
-function App() {
-  return (
-    <div className="App">
-      <Welcome name="Tony"/>
-      <Clock />
-      <Contact />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        { Component }
+        <Navigation />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="eric" />}
+        />
+        <Route path="./clock/Clock.js" component={Clock} />
+        <Route path="./contact/Contact.js" component={Contact} />
+      </div>
+    );
+  }
 }
 
 export default App;
