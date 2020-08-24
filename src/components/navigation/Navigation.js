29c86790bdf9af11868e7eb521e6861462354511
Tony Cain <r.tonycain@gmail.com>
2020-08-24 13:58:15 -0400

    inital commit

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,6 +7,7 @@ function Navigation(props) {
       <li><Link to="/">Home</Link></li>
       <li><Link to="/clock">Clock</Link></li>
       <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/Jeopardy">Jeopardy</Link></li>
     </ul>
     </div>
   );
